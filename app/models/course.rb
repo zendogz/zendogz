250190d7fc2ef3f238d6f498339f89bc1c43d225
adam <alawrence@revenuewire.com>
2014-03-26 16:41:23 -0700

    add schedule description to course show page

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ActiveRecord::Base
   has_many :enrollments
   has_many :students, through: :enrollments, source: :person
   validates :name, :description, presence: true
+
+  def schedule_obj
+    IceCube::Schedule.from_yaml(self.schedule)
+  end
 end
